b4d0a0b54cd390e24f56b6e18025828e2f932c59
AZULIRURROZ <azulista42@gmail.com>
2025-02-19 09:19:46 -0300

    correctortcswm: soporte y errores

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # «correctortcswm»
 
-Cuando guardas con la extensión SingleFile una copia de una página almacenada en linea por parte de web.archive.org, la marca temporal del archivo corresponderá al momento en el cual fue guardado el archivo, siendo distinta a la marca temporal que está en internet. Esto la cambia, al menos en el archivo, para determinado formato AÑO-MES-DÍA HOR.MIN.SEG.
+Cuando guardas con la extensión SingleFile una copia de una página almacenada en linea por parte de web.archive.org, la marca temporal del archivo corresponderá al momento en el cual fue guardado el archivo, siendo distinta a la marca temporal que está en internet. 
+
+Si el archivo a revisar tiene comentados sus datos de origen:
+> Page saved with SingleFile... url: ...
+
+este programa revisa el enlace contenido y de ahí toma la fecha original para renombrar. El formato por defecto es el siguiente, pero se puede cambiar según la siguiente sintaxis: [https://docs.rs/chrono/latest/chrono/format/strftime/index.html](https://docs.rs/chrono/latest/chrono/format/strftime/index.html). Un ejemplo, siendo %7title7 el título de la página tomado de la etiqueta <title>:
+> %Y-%m-%dT%H:%M:%SZ - %7title7
+
+Para cambiar esto, el primer argumento debe tener un texto similar al anterior. También es posible omitir ese argumento y poner directamente el archivo a renombrar, y quedaría con el formato predeterminado.
--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,53 @@
 use std::env;
 use std::fs;
 use regex::Regex;
+use chrono::{DateTime, NaiveDateTime, TimeZone, Utc};
 
 fn main() -> std::io::Result<()> {
-    let args: Vec<String> = env::args().collect();
-    for i in 1..args.len() {
-        let tmp_html = fs::read_to_string(&args[i])?;
-        let (mut tmp_date, tmp_title)  = data(tmp_html)?;
-        tmp_date.push_str("- "); tmp_date.push_str(&tmp_title);  tmp_date.push_str(".html");
-        rename(&args[i], &tmp_date)?;
+    let mut tmp_args: Vec<String> = env::args().collect();
+    let mut tmp_result = "%Y-%m-%dT%H:%M:%SZ - %7title7".to_string();
+    if tmp_args.len()>1{
+        if tmp_args.get(1).unwrap().contains("%"){
+            tmp_result = tmp_args.get(1).unwrap().to_string();
+            tmp_args.remove(1);
+        }
     }
+    for i in 1..tmp_args.len() {
+        let tmp_html = fs::read_to_string(&tmp_args[i]).expect(&tmp_args[i]);
+        let mut tmp_title  = data(tmp_html, &tmp_result).expect(&tmp_args[i]);
+        tmp_title.push_str(".html");
+        rename(&tmp_args[i], &tmp_title).expect(&tmp_args[i]);
+    }
+    println!("Ok! {} - the sum of renamed files. ", tmp_args.len()-1);
     Ok(())
 }
 
-fn data(html: String) -> Result<(String, String), std::io::Error>{
-    // let mat_date = reg_date.captures(&html).unwrap();
-    // let ret_date = mat_date.get(1).unwrap().as_str();
-    let mut ret_date: String = "1970 01 01 00 00 00 ".to_string();
-    let reg_date = Regex::new(r"(?:https:\/\/web.archive.org\/web\/)(([0-9]{4})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2}))(?:\/)").unwrap().captures(&html);
-    if !reg_date.is_none() {
-        ret_date = "".to_string();
-        let reg_dates = reg_date.unwrap();
-        for i in 2..reg_dates.len() {
-            ret_date.push_str(reg_dates.get(i).map_or("", |m| m.as_str())); ret_date.push_str(" ");
-        }
+fn data(html: String, result: &str) -> Result<String, std::io::Error>{
+    let reg_date_url = Regex::new(r"(?:(?:https|http):\/\/web.archive.org\/web\/)(([0-9]{4})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2}))(?:\/)").unwrap().captures(&html);
+    let reg_date_txt = Regex::new(r"(?:(?:saved date: )(\w{3} \w{3} \d{1,2} \d{4} \d{2}:\d{2}:\d{2} GMT(?:\+|\-)\d{4}))").unwrap().captures(&html);
+    let mut tmp_datetime: DateTime<Utc> = chrono::offset::Utc::now();
+    if !reg_date_url.is_none() {
+        let tmp_match_date = reg_date_url.unwrap().get(1).unwrap().as_str().to_string();
+        let tmp_naivedatetime = NaiveDateTime::parse_from_str(&tmp_match_date, "%Y%m%d%H%M%S").unwrap();
+        tmp_datetime = Utc.from_utc_datetime(&tmp_naivedatetime);
+     } else if !reg_date_txt.is_none() {
+        let tmp_match_date = reg_date_txt.unwrap().get(1).unwrap().as_str().to_string();
+        tmp_datetime = DateTime::parse_from_str(&tmp_match_date, "%a %b %e %Y %H:%M:%S GMT%z").unwrap().with_timezone(&Utc);
+    } 
+    let mut tmp_subtitle: String = "".to_string();
+    let reg_title= Regex::new(r"(?:<title>)(.*)(?:<\/title>)").unwrap().captures(&html);
+    if !reg_title.is_none() {
+        let tmp_match_title: regex::Captures<'_> = reg_title.unwrap();
+        let reg_untitle = Regex::new(r"[^a-zA-Z0-9 _-]").unwrap();
+        tmp_subtitle = reg_untitle.replace_all(tmp_match_title.get(1).unwrap().as_str(), "_").to_string();
     }
-    ret_date.replace_range(4..5, "-"); ret_date.replace_range(7..8, "-"); ret_date.replace_range(13..14, "_"); ret_date.replace_range(16..17, "_");
-    let reg_title: Regex = Regex::new(r"(?:<title>)(.*)(?:<\/title>)").unwrap();
-    let mat_title: regex::Captures<'_> = reg_title.captures(&html).unwrap();
-    let ret_title: String = mat_title.get(1).unwrap().as_str().to_owned();
-    Ok((String::from(ret_date), ret_title ))
+    tmp_subtitle = result.replace("%7title7", &tmp_subtitle);
+    tmp_subtitle = tmp_datetime.format(&tmp_subtitle).to_string();
+    Ok(tmp_subtitle)
 }
 
-fn rename(route: &str, replacer: &str) -> Result<bool, std::io::Error>{
-    let last: &str = route.split('/').last().unwrap();
-    fs::rename(route, route.replace(last, replacer))?; 
+fn rename(fun_route: &str, fun_replace: &str) -> Result<bool, std::io::Error>{
+    let tmp_last: &str = fun_route.split('/').last().unwrap();
+    fs::rename(fun_route, fun_route.replace(tmp_last, fun_replace))?; 
     Ok(true)
 }
